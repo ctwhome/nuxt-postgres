6903f0417dfc8dd0d7d101613f6ab26c8083922b
Jesús García <ctw@ctwhome.com>
2021-02-27 14:03:22 +0100

    Add vuetify

--- pages/index.vue
+++ pages/index.vue
@@ -1,80 +1,32 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        nuxt-postgres
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div>
+    <div class="container">
+      <v-row>
+        <Logo />
+      </v-row>
+      <v-card class="mx-auto mt-10" max-width="400" tile>
+        <v-card-title>Todos</v-card-title>
+        <v-list-item v-for="(todo, index) in todos" :key="index">
+          <v-list-item-content>
+            {{ todo.text }}
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
+export default {
+  data() {
+    return {
+      todos: [],
+    }
+  },
+  async mounted() {
+    const response = await fetch('http://localhost:8080/todos')
+    const todos = await response.json() // .then((response) => response.json())
+    this.todos = todos
+  },
+}
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
